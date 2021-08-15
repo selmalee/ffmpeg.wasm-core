c635bb3b739c37e4e5bf4f4499a1503ae1778eb1
seminelee <747191197@qq.com>
2021-08-16 00:57:02 +0800

    U build

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 set -euo pipefail
 
 EM_VERSION=2.0.8
 
 docker pull emscripten/emsdk:$EM_VERSION
+# -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
 docker run \
   --rm \
   -v $PWD:/src \
-  -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
   emscripten/emsdk:$EM_VERSION \
   sh -c 'bash ./build1.sh'
--- build1.sh
+++ build1.sh
@@ -6,34 +6,15 @@ set -e -o pipefail
 emcc -v
 
 # configure FFMpeg with Emscripten
-# Flags for code optimization, focus on speed instead
-# of size
-OPTIM_FLAGS=(
-  -O3
-)
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Use closure complier only in linux environment
-  OPTIM_FLAGS=(
-    "${OPTIM_FLAGS[@]}"
-    --closure 1
-  )
-fi
-# Convert array to string
-OPTIM_FLAGS="${OPTIM_FLAGS[@]}"
-# Root directory
-ROOT_DIR=$PWD
-# Directory to install headers and libraries
-BUILD_DIR=$ROOT_DIR/build
-CFLAGS="-I$BUILD_DIR/include $OPTIM_FLAGS"
-LDFLAGS="$CFLAGS -L$BUILD_DIR/lib"
 CONFIG_ARGS=(
   --target-os=none        # use none to prevent any os specific configurations
   --arch=x86_32           # use x86_32 to achieve minimal architectural optimization
   --enable-cross-compile  # enable cross compile
-  --extra-cflags="$CFLAGS"
-  --extra-cxxflags="$CFLAGS"
-  --extra-ldflags="$LDFLAGS"
-  --pkg-config-flags="--static"
+  --disable-x86asm        # disable x86 asm
+  --disable-inline-asm    # disable inline asm
+  --disable-stripping     # disable stripping
+  --disable-programs      # disable programs build (incl. ffplay, ffprobe & ffmpeg)
+  --disable-doc           # disable doc
   --nm="llvm-nm"
   --ar=emar
   --ranlib=emranlib
@@ -41,35 +22,23 @@ CONFIG_ARGS=(
   --cxx=em++
   --objcc=emcc
   --dep-cc=emcc
-  # 其他禁用项目
-  --disable-x86asm        # disable x86 asm
-  --disable-inline-asm    # disable inline asm
-  --disable-stripping     # disable stripping
-  --disable-programs      # disable programs build (incl. ffplay, ffprobe & ffmpeg)
-  --disable-ffmpeg
-  --disable-ffplay
-  --disable-ffprobe
-  --disable-doc           # disable doc
-  --disable-debug         # disable debug info, required by closure
-  --disable-runtime-cpudetect   # disable runtime cpu detect
-  --disable-autodetect    # disable external libraries auto detect
-  # 组件选项
-  # --disable-swresample
-  # --disable-postproc
-  # --disable-avfilter
+  # 去掉不需要的组件
+  --disable-avdevice
+  --disable-swresample
+  --disable-postproc
   --disable-network
-  --disable-w32threads
   --disable-pthreads
-  # 个别组件选项
-  --disable-devices
-  --disable-indevs
-  --disable-outdevs
-  --disable-everything
-  # 解码器
-  --enable-decoder=pcm_mulaw
-  --enable-decoder=pcm_alaw
-  --enable-decoder=adpcm_ima_smjpeg
-  --enable-decoder=aac
+  --disable-w32threads
+  --disable-os2threads
+  # 解封装，编解码等
+  --disable-everything # 减少wasm体积的关键，除了以下的组件外的个别组件都disable
+  --enable-filters
+  --enable-muxer=image2
+  --enable-demuxer=mov # mov,mp4,m4a,3gp,3g2,mj2
+  --enable-demuxer=flv
+  --enable-demuxer=h264
+  --enable-demuxer=asf
+  --enable-encoder=mjpeg
   --enable-decoder=hevc
   --enable-decoder=h264
   --enable-decoder=mpeg4
@@ -83,23 +52,21 @@ emmake make -j4
 # build ffmpeg.wasm
 mkdir -p wasm/dist
 ARGS=(
-  -I. -I./fftools # Add the specified directory to the search path for include files.
-  -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample  # Add directory to library search path
+  -I. -I./fftools # Add directory to include search path
+  -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample # Add directory to library search path
   -Qunused-arguments # Don't emit warning for unused driver arguments.
-  -o wasm/dist/ffmpeg-core.js # Write output to file.
-  fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
-  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm
-  -s USE_SDL=2                    # Specify the SDL version that is being linked against. 2 is a port of the SDL C code on emscripten-ports
-  -s MODULARIZE=1                 # use modularized version to be more flexible
-  -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
+  -o wasm/dist/ffmpeg-core.js fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c # output
+  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm # library
+  -s USE_SDL=2 # use SDL2
+  -s MODULARIZE=1 # use modularized version to be more flexible
+  -s EXPORT_NAME="createFFmpegCore" # assign export name for browser
   # -s PROXY_TO_WORKER=1                         # uses a plain Web Worker to run your main program
   # -s ENVIRONMENT='web,worker'
-  -s EXPORTED_FUNCTIONS="[_main]"  # export main and proxy_main funcs,  the main function from fftools/ffmpeg.c
-  -s EXTRA_EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
-  -s INITIAL_MEMORY=2146435072      # 2146435072 bytes = 2GB
-  -s ALLOW_MEMORY_GROWTH=1
-  -s ASSERTIONS=1
-  --post-js wasm/post-js.js
-  -O3 # optimize code and reduce code size (from 30 MB to 15 MB)
+  -s EXPORTED_FUNCTIONS="[_main]" # export main and proxy_main funcs
+  -s EXTRA_EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]" # export extra runtime methods
+  -s INITIAL_MEMORY=33554432 # 33554432 bytes = 32MB
+  -s ALLOW_MEMORY_GROWTH=1 # allows the total amount of memory used to change depending on the demands of the application
+  --post-js wasm/post-js.js # emits a file after the emitted code. use to expose exit function
+  -O3 # optimize code and reduce code size
 )
 emcc "${ARGS[@]}"
